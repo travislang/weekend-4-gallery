ea93a19efee4f1c75a231be5e178d4ec13a5c6c3
Travis Lang <tlang50@icloud.com>
2018-12-01 11:44:41 -0600

    add pictures to images folder and add them to array

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/balancing-rock.jpg', description: 'Photo of a huge rock balancing precariously in Colorado.', likes: 0 },
+    { id: 2, path: 'images/cancun-sign.jpg', description: 'Photo of me and my girlfriend behind a cancun sign in Cancun, Mexico.', likes: 0 },
+    { id: 3, path: 'images/jungle-cancun.jpg', description: 'Photo of a river through the jungle in Cancun, Mexico.', likes: 0 },
+    { id: 4, path: 'images/my-dog.jpg', description: 'Photo of my black lab named Koda.', likes: 0 },
+    { id: 5, path: 'images/utah-petroglyphs.jpg', description: 'Photo of a large wall of petroglyphs in southwest Utah.', likes: 0 },
+    { id: 6, path: 'images/voyageurs-camping.jpg', description: 'Photo of my dog and I camping in Voyageurs National Park, MN.', likes: 0 },
+    { id: 7, path: 'images/waterfall-co.jpg', description: 'Photo of me standing in front of a waterfall at hanging lake, Colorado.', likes: 0 },
+    { id: 8, path: 'images/zion-narrows.jpg', description: 'Photo of me rappeling into the narrows at Zion National Park, Utah.', likes: 0 }
 ];
 
 module.exports = galleryItems;
