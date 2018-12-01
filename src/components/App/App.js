bf25319d5c9a1e797f65e6c511c73b7f05f07040
Travis Lang <tlang50@icloud.com>
2018-12-01 12:10:29 -0600

    add get route to save image gallery to app state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of my life</h1>
-        </header>
-        <br/>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
-  }
+    state = {
+        galleryItems: []
+    }
+
+    // get all gallery images from server
+    getGallery() {
+        axios.get('/gallery')
+        .then( res => {
+            this.setState({
+                galleryItems: res.data
+            })
+        })
+        .catch( err => {
+            console.log( 'error in GET route:', err );
+        });
+    }
+
+    componentDidMount() {
+        this.getGallery();
+    }
+
+    render() {
+        console.log(this.state);
+        
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Gallery of my life</h1>
+                </header>
+                <br />
+                <p>Gallery goes here</p>
+                <img src="images/goat_small.jpg" />
+            </div>
+        );
+    }
 }
 
 export default App;
