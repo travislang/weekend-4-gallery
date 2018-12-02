3de8a2738627ba3e6676bef08abb98c3c494749d
Travis Lang <tlang50@icloud.com>
2018-12-02 17:04:51 -0600

    refactor to use DB instead of file on server

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
-
+const pool = require('../modules/pool.module');
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const sqlText = `UPDATE "images" SET "likes" = likes+1
+    WHERE "id" = $1;`;
+    pool.query( sqlText, [galleryId] )
+    .then( result => {
+        res.sendStatus( 201 );
+    }).catch( err => {
+        console.log( 'error in DB PUT query:', err );
+        res.sendStatus( 500 );
+    })
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    let sqlText = `SELECT * FROM "images" ORDER BY "id" ASC;`;
+    pool.query( sqlText )
+    .then( result => {
+        res.send( result.rows );
+    }).catch( err => {
+        console.log( 'error in DB GET query:', err );
+        res.sendStatus( 500 );
+    })
 }); // END GET Route
 
 module.exports = router;
