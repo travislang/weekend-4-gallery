247302d3b89cf4f10ca7b2e405f7bd328b908c14
Travis Lang <tlang50@icloud.com>
2018-12-02 16:29:40 -0600

    finish adding like button functionality

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -18,12 +18,25 @@ class GalleryItem extends Component {
         // set state as variable to use later
         let displayPic = this.state.displayPic;
         let displayDiv;
+        let loveDesc;
 
         // check the state for which element to display
         if ( displayPic ) {
             displayDiv = <img onClick={this.showPic} src={this.props.galleryItem.path} alt="" />
         } else {
             displayDiv = <div className='descDiv' onClick={this.hidePic}>{this.props.galleryItem.description}</div>
+        }
+        //same # of likes to a variable
+        let likes = this.props.galleryItem.likes;
+        // check how many people love the picture
+        if( likes == 0 ) {
+            loveDesc = <p>No love yet</p>
+        }
+        else if ( likes == 1 ) {
+            loveDesc = <p>{likes} person loves this</p>
+        }
+        else {
+            loveDesc = <p>{likes} people love this</p>
         }
         
         return(
@@ -32,7 +45,7 @@ class GalleryItem extends Component {
                     {displayDiv}
                 </div>
                 <button className='likeButton' onClick={ () => this.props.addLike(this.props.galleryItem.id) }>Like</button>
-                <p>{this.props.galleryItem.likes} people like this</p>
+                {loveDesc}
             </div>
         )
     }
