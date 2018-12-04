3d0bf74b2955515c8d66ce026a289d358f93f45d
Travis Lang <tlang50@icloud.com>
2018-12-03 19:51:26 -0600

    add router for pages and refactor for a home page.  Add nav styling

---
+++ src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <p>This app was built with the following technologies:</p>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Express</li>
+                    <li>Node</li>
+                    <li>Material-UI</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default About
